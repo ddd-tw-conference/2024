cb7c267887b2cd7bf039bdd07212759eb8c6d50c
Ean <wosilee@gmail.com>
2024-08-15 07:18:21 +0800

    fixed. 修正議程資訊錯誤

--- src/views/agenda.vue
+++ src/views/agenda.vue
@@ -39,7 +39,7 @@
                     <th class="pin text-xl font-semibold bg-sky-950 text-white">09:30 - 10:15<p class="text-sm mt-2">45 Minutes</p></th>
                     <td class="bg-cyan-950/20 font-semibold text-amber-300" colspan="4" style="vertical-align: middle;">
                         <h2 class="mb-3 text-xl font-semibold">開發者體驗 DevX</h2>
-                        李智樺
+                        Ruddy
                     </td>
                 </tr>
                 <tr>
@@ -64,13 +64,12 @@
                 <tr>
                     <th class="pin text-xl font-semibold bg-sky-950 text-white">11:30-12:15</th>
                     <td class="bg-cyan-950/20 font-semibold text-amber-300" style="vertical-align: middle;">
-                        開發者的矛盾<br>陳明正
+                        開發者的歹路<br>陳明正
                     </td>
                     <td class="bg-cyan-950/20 font-semibold text-lime-300" style="vertical-align: middle;">
-                        那年走過的鹽路<br>石其生
+                        Domain-Driven Transformation - how to improve the structure of legacy systems<br>Carola Lilienthal
                     </td>
                     <td class="bg-cyan-950/20 font-semibold text-cyan-500" style="vertical-align: middle;">
-                        原來開局就是一身債！<br>盧建成
                     </td>
                     <td class="bg-cyan-950/20 font-semibold text-cyan-500" style="vertical-align: middle;">
                         工作坊
@@ -85,13 +84,13 @@
                 <tr>
                     <th class="pin text-xl font-semibold bg-sky-950 text-white">13:30-14:15</th>
                     <td class="bg-cyan-950/20 font-semibold text-amber-300" style="vertical-align: middle;">
-                        面對 Legacy Code, 你有哪些選擇?<br>許煥松 Kuma
+                        面對 Legacy Code, 你有哪些選擇?<br>Kuma
                     </td>
                     <td class="bg-cyan-950/20 font-semibold text-lime-300" style="vertical-align: middle;">
-                        原來開局就是一身債！<br>盧建成
+                        那年走過的彎路<br>石其生
                     </td>
                     <td class="bg-cyan-950/20 font-semibold text-cyan-500" style="vertical-align: middle;">
-                        Fostering Socio-Technical Harmony in DDD<br>Kim Kao & Stephen Tung
+                        原來開局就一身債！從軟體邊界探索技術債的形成與團隊拓樸的設計<br>Augustion
                     </td>
                     <td class="bg-cyan-950/20 font-semibold text-cyan-500" style="vertical-align: middle;">
                         工作坊
@@ -100,10 +99,10 @@
                 <tr>
                     <th class="pin text-xl font-semibold bg-sky-950 text-white">14:15-14:30</th>
                     <td class="bg-cyan-950/20 font-semibold text-amber-300" style="vertical-align: middle;">
-                        領域驅動設計戰術內部物件的協作方式<br>黃冠霖
+                        領域驅動設計戰術內部物件的協作方式<br>神Q超人
                     </td>
                     <td class="bg-cyan-950/20 font-semibold text-lime-300" style="vertical-align: middle;">
-                        Domain Storytelling introduction<br>陳勁修
+                        Domain Storytelling introduction<br>Micheal
                     </td>
 
                     <td class="bg-cyan-950/20 font-semibold text-cyan-500" style="vertical-align: middle;">
@@ -120,13 +119,14 @@
                         From EventStorming to Event Store passing through BDD<br>Alberto Acerbis
                     </td>
                     <td class="bg-cyan-950/20 font-semibold text-lime-300" style="vertical-align: middle;">
-                        在新專案導入 DDD 的經驗談<br>劉鳳群
+                        在新專案導入 DDD 的經驗談<br>Fong
                     </td>
                     <td class="bg-cyan-950/20 font-semibold text-cyan-500" style="vertical-align: middle;">
-                        使用者研究該如何理解質性與量化的定位<br>陳啟彰
+                        使用者研究該如何理解質性與量化的定位<br>Nor
                     </td>
-                    <td class="bg-cyan-950/20 font-semibold text-lime-300" style="vertical-align: middle;"></td>
-                    <td class="bg-cyan-950/20 font-semibold text-cyan-500" style="vertical-align: middle;"></td>
+                    <td class="bg-cyan-950/20 font-semibold text-cyan-500" style="vertical-align: middle;">
+                        工作坊
+                    </td>
                 </tr>
                 </tbody>
             </table>
@@ -166,7 +166,6 @@ td {
     border: 1px dotted #464d67;
     background-clip: padding-box;
     padding: 0.5rem;
-    text-align: left;
     margin: 0;
     min-width: 22rem;
     vertical-align: top;
@@ -203,5 +202,9 @@ tbody {
 tbody th {
     position: sticky;
     left: 0;
+}
+
+tbody td{
+    text-align: center;
 }
 </style>
