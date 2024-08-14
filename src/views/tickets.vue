9089e4f3b982b0cc31ad6e0adca1d150e2873551
Ean <wosilee@gmail.com>
2024-08-15 06:14:12 +0800

    feat. 增加購票說明

--- src/views/tickets.vue
+++ src/views/tickets.vue
@@ -8,8 +8,7 @@
                 <img alt="Banner" src="/images/banner-mobile-tickets.png" />
             </div>
         </div>
-        <div class="py-10 leading-7">
-            <!--            <div class="conference">-->
+        <div class="pt-10 pb-5 leading-7">
             <h2 class="text-2xl font-bold mb-4">領域驅動設計台灣 2024 年會 - DDDesign Taiwan Conference 2024</h2>
             <p class="mb-2">
                 <span class="mr-2">🕒</span> 2024.09.13 (五) 09:00 - 17:00 (GMT+8)
@@ -36,9 +35,48 @@
                 <ticket-card :active="true" button-text="立即購票" price="800" ticket-type="重構工作坊" title="售完為止" />
                 <ticket-card :active="true" button-text="立即購票" price="800" ticket-type="應用架構設計工作坊"
                              title="售完為止" />
-
             </div>
         </div>
+
+        <!-- 加入分割線 -->
+        <hr class="border-t-2 border-gray-300 mb-8" />
+
+        <div class="p-2 rounded-lg">
+            <h2 class="text-xl font-bold mb-4">注意事項：</h2>
+            <ul class="list-disc pl-6 mb-8">
+                <li class="mb-2">
+                    主辦單位會在入場的時候檢查 Accupass 的報名 QR Code，與會者可以事先準備好，以節省入場時間。
+                </li>
+                <li class="mb-2">
+                    入場時，會進行手部的酒精消毒。
+                </li>
+                <li>
+                    參加現場活動時，活動中如有呼吸道不適，請配戴口罩，特別是與其他與會同學進行對話的時候。
+                </li>
+            </ul>
+
+            <!-- Ticket Purchasing Instructions Section -->
+            <h2 class="text-xl font-bold mb-4">購票須知：</h2>
+            <ul class="list-disc pl-6">
+                <li class="mb-2">
+                    本活動委由 Accupass 代為處理售票事宜。
+                </li>
+                <li class="mb-2">
+                    統一開立電子發票，將於活動後一週內以 Email 寄送。
+                </li>
+                <li class="mb-2">
+                    若遇不可抗拒因素，將延期舉辦並另行通知時間。主辦單位保留活動之解釋及變更權利。
+                </li>
+                <li class="mb-2">
+                    主辦單位得保留活動議程及講師之變更權利，議題與講師視實際邀約而有所變動，請以網頁最新資訊為準。
+                </li>
+                <li>
+                    退費辦法：本活動委由 Accupass 代為處理退票退款事宜，退票需於活動前 7 天提出，主辦單位將收取手續費後退還票款，活動前 7
+                    天內恕不接受退票。
+                </li>
+            </ul>
+        </div>
+
     </section>
 </template>
 
